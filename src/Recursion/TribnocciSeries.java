1bf560f29a3cab8c868d6b61abda38af514a64b8
Srinivaskoruprolu007 <srinivaskoruprolu5@gmail.com>
2022-07-25 12:19:03 +0530

    Tribnocci seris added

--- src/Recursion/TribnocciSeries.java
+++ src/Recursion/TribnocciSeries.java
@@ -1,6 +1,14 @@
 package Recursion;
 
 public class TribnocciSeries {
+    public static void main(String[] args) {
+        int n = 5;
+        System.out.println(tribnocci(n));
+    }
 
-    private static
+    private static int tribnocci(int n){
+        if(n<=1) return n;
+        if(n==2) return 1;
+        else return tribnocci(n-1)+tribnocci(n-2)+tribnocci(n-3);
+    }
 }
